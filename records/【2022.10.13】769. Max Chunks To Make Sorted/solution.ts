dbfb80b2fa90e6b3a3102f2b9b155af43fa45fdd
SoChichung <ddeadwings@gmail.com>
2022-10-13 12:58:11 +0800

    2022.10.13

--- records/【2022.10.13】769. Max Chunks To Make Sorted/solution.ts
+++ records/【2022.10.13】769. Max Chunks To Make Sorted/solution.ts
@@ -1,12 +1,24 @@
+/*
+ * @Author: SoChichung
+ * @Date: 2022-10-13 12:57:31
+ * @LastEditors: SoChichung
+ * @LastEditTime: 2022-10-13 12:57:41
+ * @Description:
+ *
+ * Copyright (c) 2022 by SoChichung ddeadwings@gmail.com, All Rights Reserved.
+ */
 function maxChunksToSorted(arr: number[]): number {
-    let n = arr.length, ans = 0
-    for (let i = 0, j = 0, min = n, max = -1; i < n; i++) {
-        min = Math.min(min, arr[i])
-        max = Math.max(max, arr[i])
-        if (j == min && i == max) {
-            ans++; j = i + 1; min = n; max = -1;
-        }
+  let n = arr.length,
+    ans = 0;
+  for (let i = 0, j = 0, min = n, max = -1; i < n; i++) {
+    min = Math.min(min, arr[i]);
+    max = Math.max(max, arr[i]);
+    if (j == min && i == max) {
+      ans++;
+      j = i + 1;
+      min = n;
+      max = -1;
     }
-    return ans
+  }
+  return ans;
 }
-
